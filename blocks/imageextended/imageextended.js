d353916d7744cd5b63f1412acb827a25f100e8bf
Smit Shah <smit.shah@teknopoint.in>
2024-06-19 15:14:08 +0530

    model link added

--- blocks/imageextended/imageextended.js
+++ blocks/imageextended/imageextended.js
@@ -1,3 +1,16 @@
 export default function decorate(block) {
     debugger;
+    if(block.classList.contains('image-bg') || block.classList.contains('image-href')){
+        if(block.classList.contains('image-bg-desktop')){
+
+        }else if(block.classList.contains('image-href-desktop') || block.classList.contains('image-href-mobile')){
+            if(block.classList.contains('image-href-desktop')){
+                var blockImageDesktop = block.querySelector('div > picture');
+                var hrefTag = document.createElement('a');
+            }
+            if(block.classList.contains('image-href-mobile')){
+                var blockImageMobile = "";
+            }
+        }
+    }
 }

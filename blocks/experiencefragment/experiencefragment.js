76c7512b01ec9e3abbc8ffb00ca05aad9934d37c
Smit Shah <smit.shah@teknopoint.in>
2024-06-22 00:44:29 +0530

    response json to text

--- blocks/experiencefragment/experiencefragment.js
+++ blocks/experiencefragment/experiencefragment.js
@@ -5,8 +5,9 @@ export default async function decorate(blocks){
     try {
         const cfURL = blocks.querySelector('a').textContent.trim();
         const fragemntData = await fetch(`${cfURL}.plain.html`);
-        const responseawait = await fragemntData.text();
-        console.log(responseawait);
+        const responseawait = await fragemntData.text();    
+        const cfDiv = document.createElement('div');
+        cfDiv.append(responseawait);
     } catch (error) {
         console.error(error);
     }

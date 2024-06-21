c0f16e784c1559c83e5e0e944fcf0f48c7e83f62
Smit Shah <smit.shah@teknopoint.in>
2024-06-22 00:24:50 +0530

    url added

--- blocks/experiencefragment/experiencefragment.js
+++ blocks/experiencefragment/experiencefragment.js
@@ -3,9 +3,10 @@ import { loadFragment } from '../fragment/fragment.js';
 
 export default async function decorate(blocks){
     try {
-        var cfURL = blocks.querySelector('a').textContent.trim();
-        var fragemntData = await fetch(`${path}.plain.html`);
-        console.log( response => response.json);
+        const cfURL = blocks.querySelector('a').textContent.trim();
+        const fragemntData = await fetch(`${cfURL}.plain.html`);
+        const responseawait = fragemntData.json();
+        console.log(responseawait);
     } catch (error) {
         console.error(error);
     }

b3ae9c100ffeec78d942538ad58fcecd7a3f4fa3
Om Gupta <om.gupta@teknopoint.in>
2024-06-25 18:40:45 +0530

    etc.clientlib issue fixed 1

--- blocks/experiencefragment/experiencefragment.js
+++ blocks/experiencefragment/experiencefragment.js
@@ -1,18 +1,21 @@
 
-export default async function decorate(blocks){
+export default async function decorate(blocks) {
     try {
         const blockProps = Array.from(blocks.children, (row) => row.firstElementChild);
-        const [xfPath , xfAuhtorURL] = blockProps;
+        const [xfPath, xfAuhtorURL] = blockProps;
         const xfURLLink = xfPath.querySelector('a').textContent.trim();
         const xfAuhorLink = xfAuhtorURL.querySelector('a').textContent.trim();
         const xfFragemntData = await fetch(`${xfAuhorLink}${xfURLLink}/master.html`);
-        let xfResponseawait = await xfFragemntData.text();    
+        let xfResponseawait = await xfFragemntData.text();
         const currUrl = new URL(location.href);
         xfResponseawait = xfResponseawait.replace(/\/etc.clientlibs/g, xfAuhorLink + "/etc.clientlibs")
         const xfDiv = document.createElement('div');
         xfDiv.innerHTML = xfResponseawait;
         blocks.innerHTML = ''
         blocks.append(xfDiv);
+        xfDiv.querySelectorAll("script").forEach(function (script) {
+            blocks.append(script);
+        })
     } catch (error) {
         console.error(error);
     }

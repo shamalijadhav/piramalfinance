9a47eb61dc59be12f1ece5b482c3e20b5e772c96
npy11 <neerajp819168@gmail.com>
2024-06-26 12:26:38 +0530

    eligibility Criteria Componet done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -167,7 +167,8 @@ async function loadingCustomCss(){
     `${window.hlx.codeBasePath}/styles/about-us-company/about-us-company.css`,
     `${window.hlx.codeBasePath}/styles/reset.css`,
     `${window.hlx.codeBasePath}/styles/key-features/key-features.css`,
-    `${window.hlx.codeBasePath}/styles/metro-cities/metro-cities.css`
+    `${window.hlx.codeBasePath}/styles/metro-cities/metro-cities.css`,
+    `${window.hlx.codeBasePath}/styles/elgibility-criteria/elgibility-criteria.css`
   ]
 
   loadCssArray.forEach( async (eachCss) => {
